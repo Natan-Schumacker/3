fd70f40f025ba1f6f8596164c57bb984dfca05ac
Natan-Schumacker <160528091+Natan-Schumacker@users.noreply.github.com>
2024-06-19 12:15:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Você é o líder de uma das nações mais poderosas do mundo, eleito na última eleição. Apesar de toda a infraestrutura e PIB de seu país, ele é um dos que mais produz poluição e tambem um dos que mais contribui para o aquecimento global.",
+        enunciado: "Você é o líder de uma das nações mais poderosas do mundo, eleito na última eleição. Apesar de toda a infraestrutura e PIB de seu país, ele é um dos que mais produz poluição e também um dos que mais contribui para o aquecimento global.",
         alternativas: [
             {
                 texto: "Isso é assustador!",
@@ -32,28 +32,28 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Você é chamado alguns mêses depois de implementar suas poilíticas para uma convenção sobre mudandaças climáticas, eles perguntam e cobram sobre a situação, mas exclusivamente na parte de como os empresarios entraram no projeto do governo sobre Sustentabilidade ambiental",
+        enunciado: "Você é chamado alguns meses depois de implementar suas poilíticas para uma convenção sobre mudanças climáticas, eles perguntam e cobram sobre a situação, mas exclusivamente na parte de como os empresários entraram no projeto do governo sobre Sustentabilidade ambiental",
         alternativas: [
             {
-                texto: "você fala sobre politicas que seram implantadas nas empresas como por exemplo: obrigação legilativa para empresas doarem uma porcentagem de seu terreno industrial/comercial para replantio de vegetação nativa",
-                afirmacao: "e na hora para se pronunciar sobre o asunto disse que estava trabalhando arudamente para acabar com o problema."
+                texto: "você fala sobre políticas que serão implantadas nas empresas como por exemplo: obrigação legislativa para empresas doarem uma porcentagem de seu terreno industrial/comercial para replantio de vegetação nativa",
+                afirmacao: "e na hora para se pronunciar sobre o assunto disse que estava trabalhando arduamente para acabar com o problema."
             },
             {
-                texto: "Você fala somente que eles seram mais cobrados sobre as legislações atuais, pois mais politicas seriam prejudiais a economia e iriam diminuir a capacidade de crescimento social e economico da polulação",
+                texto: "Você fala somente que eles seram mais cobrados sobre as legislações atuais, pois mais políticas seriam prejudiais a economia e iriam diminuir a capacidade de crescimento social e econômico da população",
                 afirmacao: "e na hora para se pronunciar sobre o assunto disse que estava fazendo o possível para resolver o problema."
             }
         ]
     },
     {
-        enunciado: "Depois da conferência você sente que as suas decisões estão repercutindo demais e você pensa uma ultima vez Será que essas desisões foram corretas? você acaba sendo reeleito e pensa se vc deveria continuar com as mesmas leis e pensa no que fara nesse novo mandato",
+        enunciado: "Depois da conferência você sente que as suas decisões estão repercutindo demais e você pensa uma última vez. Será que essas decisões foram corretas? você acaba sendo reeleito e pensa se você deveria continuar com as mesmas leis e pensa no que fara nesse novo mandato",
         alternativas: [
             {
                 texto: "Diminuir a poluição",
-                afirmacao: "Depois de repensar sobre suas politícas ambientais no começo de seu segundo mandato ele decidiu retomar com politicas decisiva que fizeram o país diminuir consideravelmente as emições de poluentes, que no futuro se tornaram referencia no mundo inteiro e os outros países que ignoraram essas questões sofrem por causa do ar e água e terras contaminados"
+                afirmacao: "Depois de repensar sobre suas políticas ambientais no começo de seu segundo mandato ele decidiu retomar com as políticas decisivas que fizeram o país diminuir consideravelmente as emições de poluentes, que no futuro se tornaram referência no mundo inteiro e os outros países que ignoraram essas questões sofrem por causa do ar, águas e terras contaminadas"
             },
             {
                 texto: "Diminuir pobreza",
-                afirmacao: " Depois de repensar suas politicas para eu segundo mandato você decide focar na economia, mais especificamente no fim  da pobreza, apesar de um sucesso grandioso, as pesoas agora sofrem diariamente com a poluição diminuindo a expecttativa de vida da população, principalmente daqueles poucos que ainda continuam pobres"
+                afirmacao: " Depois de repensar suas políticas para seu segundo mandato você decide focar na economia, mais especificamente no fim  da pobreza, apesar de um sucesso grandioso, as pesoas agora sofrem diariamente com a poluição diminuindo a expecttativa de vida da população, principalmente daqueles poucos que ainda continuam pobres"
             }
         ]
     },
